df3abdab0ea0b9c6c098ed4948139dced8d198d2
Petra Vrablecova <petra.vrablecova@telekom.com>
2025-12-02 10:23:00 +0100

    day1-2

--- day_02/task_02.py
+++ day_02/task_02.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2025/day/2
+# https://adventofcode.com/2025/day/2#part2
 
 import re
 
@@ -10,6 +10,6 @@ for id_range in id_ranges:
     start, stop = map(int, id_range.split("-"))
     id_range = list(map(str, range(start, stop + 1)))
 
-    [invalid_ids.append(int(id)) for id in id_range if re.match(r"^(\d+)\1$", id)]
+    [invalid_ids.append(int(id)) for id in id_range if re.match(r"^(\d+)\1+$", id)]
 
 print(sum(invalid_ids))
