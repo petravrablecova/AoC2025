76add009c77198acad4486c317d0b96078eeb9c1
Petra Vrablecova <petra.vrablecova@telekom.com>
2025-12-02 10:23:00 +0100

    day1-2

--- day_02/task02.py
+++ day_02/task02.py
@@ -5,11 +5,11 @@ import re
 with open('input.txt') as f:
     id_ranges = f.read().split(',')
 
-valid_ids = list()
+invalid_ids = list()
 for id_range in id_ranges:
     start, stop = map(int, id_range.split('-'))
     id_range = list(map(str, range(start, stop+1)))
 
-    [valid_ids.append(int(id)) for id in id_range if re.match(r'^(\d+)\1+$', id)]
+    [invalid_ids.append(int(id)) for id in id_range if re.match(r'^(\d+)\1+$', id)]
 
-print(sum(valid_ids))
+print(sum(invalid_ids))
